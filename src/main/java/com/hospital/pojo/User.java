aec2069a67140eed6c64e1a67a4df33261fa391e
skyhao <1021852155@qq.com>
2016-05-20 10:53:41 +0800

    完成pojo的user类

--- src/main/java/com/hospital/pojo/User.java
+++ src/main/java/com/hospital/pojo/User.java
@@ -3,6 +3,58 @@ package com.hospital.pojo;
 
 public class User {
 
+    private Integer id;
+    private String username;
+    private String password;
+    private String tel;
+    private String realname;
+    private String role;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
